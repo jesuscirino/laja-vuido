679945a90fbde6ed7372f9b076f82ab2acb01aaf
jesuscirino <jesuscirino@gmail.com>
2018-12-15 23:47:40 -0600

    Probando Buttons

--- laja/src/MainWindow.vue
+++ laja/src/MainWindow.vue
@@ -1,7 +1,11 @@
 <template>
   <Window title="laja" width="400" height="100" margined v-on:close="exit">
-    <Box>
+    <Box padded>
       <Text>Welcome to your Vuido application!</Text>
+      <Box horizontal padded>
+        <Button stretchy>OK</Button>
+        <Button stretchy>Cancel</Button>
+      </Box>
     </Box>
   </Window>
 </template>
